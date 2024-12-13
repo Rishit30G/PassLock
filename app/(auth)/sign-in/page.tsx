3398cecc9b431933ee3bc87c5082f869ce44e5ec
Rishit Gupta <74411873+Rishit30G@users.noreply.github.com>
2024-12-13 11:52:14 +0530

    Add Auth Form Component

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,16 +1,8 @@
-import React from 'react'
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
-import { ConfettiButton } from '@/components/ui/confetti'
+import AuthForm from "../_components/Form"
 
 const SignInForm = () => {
   return (
-    <div className="space-y-4 !mt-8 flex flex-col">
-            <Input type="email" placeholder="Email" />
-            <Input type="password" placeholder='Password' />
-            <Button className="w-full">Sign In</Button>
-            <p className='text-center text-sm text-gray-500'>Don't have an account? <a href="/sign-up">Sign Up</a></p>
-    </div>
+    <AuthForm formType="sign-in"/>
   )
 }
 
