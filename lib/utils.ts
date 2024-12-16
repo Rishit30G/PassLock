b04f8ec71579495c951bf46c0cd938f4dd0c187e
Rishit Gupta <74411873+Rishit30G@users.noreply.github.com>
2024-12-16 17:01:58 +0530

    Add encryption and decryption

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,9 +1,39 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import crypto from "crypto";
+
+const ENCRYPTION_KEY = Buffer.from('01234567890123456789012345678901', "utf8");
+const IV_LENGTH = 16; // Initialization vector length for AES
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export const parseStringify = (value: unknown) =>
   JSON.parse(JSON.stringify(value));
+
+// Encrypt function
+export function encrypt(text: string): string {
+  const iv = crypto.randomBytes(IV_LENGTH); // Generate a random IV
+  const cipher = crypto.createCipheriv("aes-256-cbc", ENCRYPTION_KEY, iv);
+  let encrypted = cipher.update(text, "utf8", "hex");
+  encrypted += cipher.final("hex");
+  return iv.toString("hex") + ":" + encrypted; // Return IV + Encrypted text
+}
+
+// Decrypt function
+export function decrypt(encryptedText: string): string {
+  const [iv, encrypted] = encryptedText.split(":");
+  const decipher = crypto.createDecipheriv(
+    "aes-256-cbc",
+    ENCRYPTION_KEY,
+    Buffer.from(iv, "hex")
+  );
+  let decrypted = decipher.update(encrypted, "hex", "utf8");
+  decrypted += decipher.final("utf8");
+  return decrypted; // Return original text
+}
+
+export const maskPassword = (password: string) => {
+  return '*'.repeat(password.length);
+};
