f7ec17e6087ddfd851740f76084366e4ec2b22bd
Rishit Gupta <74411873+Rishit30G@users.noreply.github.com>
2025-01-01 15:55:26 +0530

    Update arcjet.ts with new arcjet instances

--- lib/arcjet/arcjet.ts
+++ lib/arcjet/arcjet.ts
@@ -1,16 +1,40 @@
-import arcjet, {tokenBucket} from "@arcjet/next"; 
+import arcjet, { tokenBucket } from "@arcjet/next";
 
-const aj = arcjet({
-    key: process.env.ARCJET_KEY!,
-    characteristics: ["userId"],
-    rules: [
-        tokenBucket({
-            mode: "LIVE", 
-            refillRate: 2, 
-            interval: 86400,
-            capacity: 2,
-        })
-    ]
+export const ajUpdate = arcjet({
+  key: process.env.ARCJET_UPDATE_KEY!, 
+  characteristics: ["userId"],
+  rules: [
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 50, 
+      interval: 86400, 
+      capacity: 50,
+    }),
+  ],
 });
 
-export default aj; 
+export const ajDelete = arcjet({
+  key: process.env.ARCJET_DELETE_KEY!, 
+  characteristics: ["userId"],
+  rules: [
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 50, 
+      interval: 86400, 
+      capacity: 50,
+    }),
+  ],
+});
+
+export const ajCreate = arcjet({
+  key: process.env.ARCJET_CREATE_KEY!, 
+  characteristics: ["userId"],
+  rules: [
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 100, 
+      interval: 86400, 
+      capacity: 100,
+    }),
+  ],
+});
