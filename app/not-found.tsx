719a62730d38868bf6e48782ad32408acbf4a4d5
Rishit Gupta <74411873+Rishit30G@users.noreply.github.com>
2025-01-07 15:24:35 +0530

    Update Not Foun

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -14,7 +14,7 @@ const page = () => {
     <div className='min-h-screen flex justify-center items-center flex-col gap-4'>
         <h1 className="poppins-extralight text-6xl text-center">Not Found: 404</h1>
         <Image src="/404.jpg" width={500} height={500} alt="404" />
-        <h2 className='poppins-light'>You are here faster than we could build it 🥹</h2>
+        <h2 className='poppins-light'>You are here faster than we could build it 👀</h2>
         <ConfettiButton>🎊</ConfettiButton> 
     </div>
     </>
